11014deb477276a0ab67f991659da46f14370a48
Nspired1 <don.spire1@gmail.com>
2020-09-27 22:26:05 -0700

    connect to MongoDB Atlas

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((err) => {
+    console.log("ERROR:", err.message);
+  });
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 6000;
 const IP = process.env.IP;
 
-app.get("/", function (req, res) {
-  res.send("API Running");
+//connect to database
+connectDB;
+
+app.get("/", (req, res) => {
+  res.send(`API Running`);
 });
 
-//per scripts in package.json run server with command: npm run server
-
-app.listen(PORT, function () {
+//as per scripts in package.json run nodemon server with command: npm run server
+app.listen(PORT, IP, () => {
   console.log(
     `Server started and listening intenely on port ${PORT} and ip ${IP}`
   );
